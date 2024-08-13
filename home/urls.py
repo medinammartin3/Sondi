13523a4df40d63c847a6aef28b6a32964bbf0d29
Martin <medinammartin3@gmail.com>
2024-08-12 21:08:28 -0400

    Easter Egg

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "home"
 urlpatterns = [
     path("", views.HomeTemplateView.as_view(), name="index"),
-    path("about/", views.HomeTemplateView.as_view(template_name="home/about.html"), name="about")
+    path("about/", views.HomeTemplateView.as_view(template_name="home/about.html"), name="about"),
+    path("pachito&chipie/", views.HomeTemplateView.as_view(template_name="home/pachito&chipie.html"), name="about"),
 ]
