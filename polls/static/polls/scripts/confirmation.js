586211fdc7f54ed380af07e3d3640f8e41a8c75a
Martin <medinammartin3@gmail.com>
2024-08-13 15:25:04 -0400

    Share with link

--- polls/static/polls/scripts/confirmation.js
+++ polls/static/polls/scripts/confirmation.js
@@ -2,11 +2,24 @@ function copyCode(){
     let code = document.getElementById("code").innerHTML;
     navigator.clipboard.writeText(code);
 
-    var tooltip = document.getElementById("myTooltip");
+    var tooltip = document.getElementById("myTooltipCode");
     tooltip.innerHTML = "Copied!";
 }
 
 function copyCodeOut() {
-    var tooltip = document.getElementById("myTooltip");
+    var tooltip = document.getElementById("myTooltipCode");
+    tooltip.innerHTML = "Copy";
+}
+
+function copyLink(){
+    let link = document.getElementById("link").innerHTML;
+    navigator.clipboard.writeText(link);
+
+    var tooltip = document.getElementById("myTooltipLink");
+    tooltip.innerHTML = "Copied!";
+}
+
+function copyLinkOut() {
+    var tooltip = document.getElementById("myTooltipLink");
     tooltip.innerHTML = "Copy";
 }
